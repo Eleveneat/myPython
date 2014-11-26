bee46bf9e01de9a42a127d38923076af3cd65072
Eleven <eleveneat@gmail.com>
2014-11-26 17:53:47 +0800

    Ex7 final version.
    After doing the exercise, I am more familiar with tornado. Amazing
    framework.

--- Ex7/buyagrade.py
+++ Ex7/buyagrade.py
@@ -1,7 +1,7 @@
 #
 #    Filename: buyagrade.py
 #    Description: None.
-#    Last modified: 2014-11-25 21:50
+#    Last modified: 2014-11-26 17:49
 #
 #    Created by Eleven on 2014-11-23
 #    Email: eleveneat@gmail.com
@@ -17,41 +17,6 @@ import tornado.web
 
 from tornado.options import define, options
 define("port", default=8888, help="run on the given port", type=int)
-
-class MainHandler(tornado.web.RequestHandler):
-    # @tornado.web.authenticated
-    def get(self):
-        self.render("buyagrade.html")
-    def post(self):
-        name = self.get_argument("Name", 1)
-        section = self.get_argument("Section")
-        creditCardNum = self.get_argument("CreditCardNum")
-        creditCardType = self.get_argument("CreditCardType")
-        print type(creditCardType)
-
-        if(len(name)*len(creditCardNum)):
-            if creditCardType == "Visa":
-                pattern = re.compile(r'4{1}\d{15}|4{1}\d{3}(-\d{4}){3}')
-            else:
-                pattern = re.compile(r'5{1}\d{15}|5{1}\d{3}(-\d{4}){3}')
-            match = pattern.match(creditCardNum)
-            if match and isLuhnValid(creditCardNum):
-                writeSuckersFile(name, section, creditCardNum, creditCardType)
-                fs = open("static/txt/suckers.txt", "r")
-                fileMessages = fs.read()
-                fs.close()
-
-                self.render("sucker.html", suckerName=name,
-                    suckerSection=section,
-                    suckerCreditCardNum=creditCardNum,
-                    suckerCreditCardType=creditCardType,
-                    suckerFileMessages=fileMessages)
-            else:
-                reason = "You didn't provide a valid card number."
-                self.render("sorry.html", errorReason=reason)
-        else:
-            reason = "You didn't fill out the form completely."
-            self.render("sorry.html", errorReason=reason)
 
 def isLuhnValid(creditCardNum):
     sum = 0
@@ -79,16 +44,44 @@ def writeSuckersFile(name, section, creditCardNum, creditCardType):
     fs.write(message)
     fs.close()
 
-class SuckerHandler(MainHandler):
+class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        pass
-class SorryHandler(MainHandler):
-    def get(self):
-        pass
+        self.render("buyagrade.html")
+
+class SuckerHandler(tornado.web.RequestHandler):
+    def post(self):
+        name = self.get_argument("Name", 1)
+        section = self.get_argument("Section")
+        creditCardNum = self.get_argument("CreditCardNum")
+        creditCardType = self.get_argument("CreditCardType")
+
+        if(len(name)*len(creditCardNum)):
+            if creditCardType == "Visa":
+                pattern = re.compile(r'4{1}\d{15}|4{1}\d{3}(-\d{4}){3}')
+            else:
+                pattern = re.compile(r'5{1}\d{15}|5{1}\d{3}(-\d{4}){3}')
+            match = pattern.match(creditCardNum)
+            if match and isLuhnValid(creditCardNum):
+                writeSuckersFile(name, section, creditCardNum, creditCardType)
+                fs = open("static/txt/suckers.txt", "r")
+                fileMessages = fs.read()
+                fs.close()
+
+                self.render("sucker.html", suckerName=name,
+                    suckerSection=section,
+                    suckerCreditCardNum=creditCardNum,
+                    suckerCreditCardType=creditCardType,
+                    suckerFileMessages=fileMessages)
+            else:
+                reason = "You didn't provide a valid card number."
+                self.render("sorry.html", errorReason=reason)
+        else:
+            reason = "You didn't fill out the form completely."
+            self.render("sorry.html", errorReason=reason)
 if __name__ == "__main__":
     tornado.options.parse_command_line()
     application = tornado.web.Application(
-    [(r"/", MainHandler), (r"/sucker", SuckerHandler), (r"/sorry", SorryHandler)],
+    [(r"/", MainHandler), (r"/sucker", SuckerHandler)],
     template_path = os.path.join(os.path.dirname(__file__), "templates"),
     static_path = os.path.join(os.path.dirname(__file__), "static"),
     debug = True
